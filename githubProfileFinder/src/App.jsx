821d1eab3f9c331ab3262ea71a61cf745536eafc
Kanak <kanakkumarmahala000@gmail.com>
2024-08-07 23:32:13 +0530

    githup-profile-finder

---
+++ githubProfileFinder/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import GithubProfileFinder from './components/GithubFinder'
+
+function App() {
+  
+
+  return (
+    <>
+      <GithubProfileFinder/>
+    </>
+  )
+}
+
+export default App
---
+++ githubProfileFinder/src/components/GithubFinder.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import User from "./User";
+
+
+
+export default function GithubProfileFinder() {
+  
+    const [username , setUsername] = useState('kanakk365');
+    const [data , setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    function handleSubmit(){
+        fetchData()
+    }
+    
+    async function fetchData(){
+
+        setLoading(true);
+        const res = await fetch(`https://api.github.com/users/${username}`);
+        const data = await res.json();
+        if(data){setData(data)}
+        console.log(data)
+        setLoading(false)
+    }
+    
+    useEffect(()=>{
+        fetchData()
+    },[])
+    
+    if(loading) {
+        return(  <h1> Loading Please wait </h1> )
+    }
+    
+    return (
+    <div className="profile-container">
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter Username"
+          name="search-by-username"
+          value={username}
+          onChange={(event)=> setUsername(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {data ? <User user={data}/> : null}
+    </div>
+  );
+}
