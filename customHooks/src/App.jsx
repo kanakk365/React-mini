efbe0f935b3789ab95534544be416da7588a62d0
Kanak <kanakkumarmahala000@gmail.com>
2024-08-11 23:44:26 +0530

    --

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,7 +1,6 @@
 
 import './App.css'
-import UseFetch from './components/use-fetch'
-import UseOnclickTest from './components/Use-OnclickOutside/test'
+
 import UseWindowResizeTest from './components/UseWindowResize/text'
 
 
